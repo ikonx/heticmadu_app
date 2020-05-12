7300dc9eb5279165e54ebcb3b1a7a799a02ed537
Dorian Taing <doriantaing@dorian-macbook-air.home>
2020-05-12 17:34:23 +0200

    add theme

---
+++ src/styleGuide/Fonts.tsx
@@ -0,0 +1,7 @@
+export default {
+  DMSans: {
+    regular: 'DMSans-Regular, Helvetica, sans-serif',
+    medium: 'DMSans-Medium, Helvetica, sans-serif',
+    bold: 'DMSans-Bold, Helvetica, sans-serif',
+  }
+}
---
+++ src/styleGuide/Theme.tsx
@@ -0,0 +1,13 @@
+import Colors from "./Colors";
+import Icons from "./Icons";
+import Illustrations from "./Illustrations";
+import Fonts from "./Fonts";
+import Shadow from "./Shadow";
+
+export default {
+  Colors,
+  Icons,
+  Illustrations,
+  Fonts,
+  Shadow
+};
