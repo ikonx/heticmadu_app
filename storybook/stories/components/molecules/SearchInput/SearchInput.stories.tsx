7945fcf9ddddc60ddb3ac69bf33cdd9ec68da8af
doriantaing <doriantaing77@hotmail.fr>
2020-06-02 14:34:37 +0200

    add all molecules to storybook #52

---
+++ storybook/stories/components/molecules/SearchInput/SearchInput.stories.tsx
@@ -0,0 +1,12 @@
+import React, { FunctionComponent } from 'react';
+import SearchInput from '@components/molecules/SearchInput/SearchInput';
+
+interface Props {}
+
+const SearchInputStories: FunctionComponent<Props> = () => {
+  return (
+    <SearchInput />
+  );
+};
+
+export default SearchInputStories;
