4b6312b5617b0ec262d0b7c62ecb6727891e52df
Theo LARUE <Theo@MacBook-Pro-15R-de-Xavier-Badaire-3.local>
2020-06-02 17:12:42 +0200

    avatar atom #64

--- src/components/screens/HomeScreen/HomeScreen.tsx
+++ src/components/screens/HomeScreen/HomeScreen.tsx
@@ -4,6 +4,7 @@ import Text from '@src/components/atoms/Typography/Text/Text';
 import { SafeAreaView } from 'react-navigation';
 import PoiCard from '@src/components/molecules/PoiCard/PoiCard';
 import { poisData } from '@src/utils/mocks/pois.data';
+import Avatar from '@src/components/atoms/Avatar/Avatar';
 
 interface Props {}
 
@@ -14,6 +15,7 @@ const HomeScreen = (_: Props) => {
         <Text variant="default">Wesh alors</Text>
       </View>
       <PoiCard poi={poisData[0]} gotBorder />
+      <Avatar />
     </SafeAreaView>
   );
 };
--- src/styleGuide/Borders.ts
+++ src/styleGuide/Borders.ts
@@ -2,5 +2,7 @@ import Colors from '@styleGuide/Colors';
 
 export default {
   mainBorder: '1px solid rgba(28, 28, 28, 0.25)',
-  lightBorder: `1px solid ${ Colors.secondaryGrey }`,
+  lightBorder: `1px solid ${Colors.secondaryGrey}`,
+  avatarBorder: `1px solid ${Colors.mainPurple}`,
+  blackBorder: `2px solid ${Colors.mainBlack}`,
 };
