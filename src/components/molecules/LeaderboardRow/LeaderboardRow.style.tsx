61ac091caa99f09c14ca46538bd5e1a82ff1b211
doriantaing <doriantaing77@hotmail.fr>
2020-06-27 19:38:10 +0200

    add leaderboardRow molecule #96

---
+++ src/components/molecules/LeaderboardRow/LeaderboardRow.style.tsx
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+import List from '@components/atoms/List/List';
+
+export const RowContainer = styled(List)`
+  align-items: center;
+  justify-content: space-between;
+  width: 100%;
+`;
+
+export const RowLeft = styled(List)`
+  align-items: center;
+`;
